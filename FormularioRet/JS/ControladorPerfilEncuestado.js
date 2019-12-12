2f192acdbea10631faf429582528acda342e7c7a
JMVice <43189054+JMVice@users.noreply.github.com>
2019-12-12 10:58:48 -0600

    Bug fix

--- FormularioRet/JS/ControladorPerfilEncuestado.js
+++ FormularioRet/JS/ControladorPerfilEncuestado.js
@@ -1,11 +1,11 @@
 var MODULO_A_OCULTAR_ID = "N/A yet",
-        MODULO_A_MOSTRAR_ID = "N/A yet",
-        VALOR_SELECCIONADO_PEFIL_ENCUESTADO = "N/A yet";
+    MODULO_A_MOSTRAR_ID = "N/A yet",
+    VALOR_SELECCIONADO_PEFIL_ENCUESTADO = "N/A yet";
 
 const RUTA_CONTENIDO_DIV_HOGAR = "TXT/CaracterizacionDelUsoDeCamasEnElHogar-CONTENIDO.txt",
-        RUTA_CONTENIDO_DIV_NEGOCIO = "TXT/CaracterizacionDelUsoDeCamasEnElNegocio-CONTENIDO.txt",
-        MODULO_CARACTERIZACION_CAMAS_NEGOCIO_ID = "CaracterizacionDelUsoDeCamasEnElNegocio",
-        MODULO_CARACTERIZACION_CAMAS_HOGAR_ID = "CaracterizacionDelUsoDeCamasEnElHogar";
+    RUTA_CONTENIDO_DIV_NEGOCIO = "TXT/CaracterizacionDelUsoDeCamasEnElNegocio-CONTENIDO.txt",
+    MODULO_CARACTERIZACION_CAMAS_NEGOCIO_ID = "CaracterizacionDelUsoDeCamasEnElNegocio",
+    MODULO_CARACTERIZACION_CAMAS_HOGAR_ID = "CaracterizacionDelUsoDeCamasEnElHogar";
 
 function ControladorPerfilEncuestado_Funcion() {
     VALOR_SELECCIONADO_PEFIL_ENCUESTADO = TREAR_VALOR_DEL_TAG_DADO_SU_ID("PERFIL_ENCUESTADO");
@@ -34,6 +34,7 @@ function ocultar_modulo(id_div) {
 function mostrar_modulo(id_div) {
     if (id_div === MODULO_CARACTERIZACION_CAMAS_NEGOCIO_ID) {
         CARGA_AJAX(id_div, RUTA_CONTENIDO_DIV_NEGOCIO);
+        contador_negocio = 1;
     } else {
         //Si no es del negocio, es del hogar.
         CARGA_AJAX(id_div, RUTA_CONTENIDO_DIV_HOGAR);
